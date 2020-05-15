55447eb91aeef97a99f3351fd5afd090b8c55b5f
Josh Gearheart <jgearheart@CN11965.local>
2020-05-15 16:04:07 -0700

    change route structure

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -38,7 +38,7 @@ router.route("/:id").delete((req, res) => {
     .catch((err) => res.status(400).json("Error: " + err));
 });
 
-router.route("/update/:id").post((req, res) => {
+router.route("/:id/update").post((req, res) => {
   Exercise.findById(req.params.id)
     .then((exercise) => {
       exercise.username = req.body.username || exercise.username;
